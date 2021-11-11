1f5de21a5d43f748467d150f559a641f62d9da42
AbhijitAher <seabhi.aher@gmail.com>
2021-11-12 01:31:49 +0530

    added sliding element to Home page

--- public/html/components/navbar.js
+++ public/html/components/navbar.js
@@ -1,32 +1,32 @@
 function navbar(){
-    return `<nav class="bg-black sticky top-0 text-white h-20 flex">
+    return `<nav class="bg-black sticky top-0 text-white h-20 flex z-70">
     <div class="container inline-flex mr-10 pl-7 items-center w-10/12">
-        <a class="w-12 h-12 mx-4" href="#">
+        <a class="w-12 h-12 mx-4" href="index.html">
             <img src="./images/ZEE5_logo.svg" alt="">
         </a>
         <a class="px-3 text-sm"  href="index.html">
             <p class="border-b-2 border-white p-1">Home</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">TV Shows</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">TV Shows</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">Movies</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">Movies</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">Web Series</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">Web Series</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">News</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">News</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">Premium</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">Premium</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">Live TV</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">Live TV</p>
         </a>
         <a class="px-3" href="#">
-            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition ease-in hover:border-white">ZEEPLEX</p>
+            <p class="text-sm whitespace-nowrap p-1 border-black border-b-2 transition duration-250 ease-in hover:border-white">ZEEPLEX</p>
         </a>
         <a class="px-3 text-xs" href="#">
             <span class="material-icons p-1">
@@ -37,16 +37,18 @@ function navbar(){
     </div>
     <div class="container inline-flex items-center w-4/12">
     
-        <a class="px-2" href="#">
+        <a class="px-3" href="#">
             <span class="material-icons p-1">
                 search
             </span>
         </a>
-        <a class="px-2" href="#">
+        
+        <a class="px-3" href="#">
             <span class="material-icons p-1">
                 translate
             </span>
         </a>
+
 
         <button type="submit" class="ml-3 bg-black p-2 border border-white rounded-md text-xs font-bold transition duration-500 hover:bg-white hover:text-black"> <a href="sign_in.html" target="_blank"> LOGIN </a></button>
         
@@ -54,6 +56,12 @@ function navbar(){
             <img class="w-5 mr-1" src="./images/crown.png"  alt=""/>
             <span>BUY PLAN</span>
         </button>
+
+        <a class="px-3" href="#">
+            <span class="material-icons p-1">
+                menu
+            </span>
+        </a>
     </div>
 </nav>`
 }
